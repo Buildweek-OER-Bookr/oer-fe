1ec3ebf8ec0245273934c098091100a21cf7b43a
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-23 17:32:13 +0100

    Fixed router bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OERBookr from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Router>
-		<Route exact path="/" component={OERBookr} />
+		<OERBookr />
 	</Router>,
 	document.getElementById('root')
 );
