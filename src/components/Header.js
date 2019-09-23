c054642936dee8d0ec252de036de40c9f6a1a09a
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-23 19:34:35 +0100

    Styled Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import logo from '../images/logo.png';
+const StyledHeader = styled.header`
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	z-index: 11;
+	width: 250px;
+	border-right: 1px solid ${props => props.theme.gray};
+	.logo {
+		padding: 20px;
+	}
+	.logo + a {
+		border-top: 1px solid ${props => props.theme.gray};
+	}
+	a {
+		display: block;
+		font-size: 1.5rem;
+		line-height: 4.5rem;
+		padding: 0.5rem 20px 0;
+		border-bottom: 1px solid ${props => props.theme.gray};
+	}
+`
 const Header = (props) => {
+	const menuUrls = [
+		{
+			name: 'Book List',
+			url: '/dashboard',
+		},
+		{
+			name: 'Book Details',
+			url: '/dashboard/1',
+		},
+		{
+			name: 'Logout',
+			url: '/logout',
+		}
+	];
 	return(
-		<header>
+		<StyledHeader>
 			<div className="logo">
 				<img src={logo} alt=""/>
 			</div>
-		</header>
+			{menuUrls.map(menu => (
+				<NavLink key={menu.url} to={menu.url} activeClassName="active">{menu.name}</NavLink>
+			))}
+		</StyledHeader>
 	)
 }
 
