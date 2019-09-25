5bf3e045b6b5dff02ad011d3f6154a9176b217c0
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-25 11:36:39 +0100

    Started responsive header  | Populated menu and contacts

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,74 +3,130 @@ import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../images/logo.png';
 
+import { connect } from 'react-redux';
+import { getData } from './../actions/index';
 const StyledHeader = styled.header`
 	position: fixed;
+	z-index: 11;
 	top: 0;
 	left: 0;
-	bottom: 0;
-	z-index: 11;
-	width: 250px;
+	width: 100%;
+	height: 80px;
+	padding: 0 75px;
+	display: flex;
+	flex-flow: row nowrap;
 	background-color: ${props => props.theme.white};
-	border-right: 1px solid ${props => props.theme.blue};
+	@media screen and (min-width: 1024px) {
+		padding: 0;
+		border-bottom: none;
+		flex-flow: column nowrap;
+		width: 250px;
+		height: 100%;
+		border-right: 1px solid ${props => props.theme.blue};
+	}
 	.logo {
-		padding: 20px;
-		& + a {
-			border-top: 1px solid ${props => props.theme.blue};
-		}
-	}
-	& > a {
-		display: block;
+		padding: 10px 20px 10px 0px;
 		width: 100%;
-		font-size: 1.5rem;
-		line-height: 4.5rem;
-		padding: 0.5rem 20px 0;
-		color: ${props => props.theme.blue};
-		border-bottom: 1px solid ${props => props.theme.blue};
-		&.active {
-			color: ${props => props.theme.white};
-			background-color: ${props => props.theme.blue};
+		max-width: 80px;
+		flex-basis: 80px;
+		@media screen and (min-width: 1024px) {
+			padding: 20px;
+			max-width: 100%;
+			flex-basis: auto;
 		}
 	}
-	.share {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		padding: 20px;
+	.menu {
+		display: flex;
+		flex-flow: row nowrap;
+		flex-grow: 1;
+		font-family: ${props => props.theme.fonts.josefin};
+		@media screen and (min-width: 1024px) {
+			flex-flow: column nowrap;
+		}
+		a {
+			font-size: 1.25rem;
+			line-height: 72px;
+			padding: 8px 20px 0;
+			color: ${props => props.theme.blue};
+			&.active {
+				color: ${props => props.theme.white};
+				background-color: ${props => props.theme.blue};
+			}
+			@media screen and (min-width: 1024px) {
+				width: 100%;
+				font-size: 1.5rem;
+				line-height: 4.5rem;
+				padding: 0.5rem 20px 0;
+				border-bottom: 1px solid ${props => props.theme.blue};
+				&:first-child {
+					border-top: 1px solid ${props => props.theme.blue};
+				}
+			}
+		}
+	}
+	.contacts {
+		.list {
+			display: flex;
+			flex-flow: row nowrap;
+			justify-content: space-between;
+		}
+		a {
+			flex-grow: 1;
+			padding: 0 10px;
+			text-align: center;
+			color: ${props => props.theme.blue};
+			transition: all .33s ease-in-out;
+			i:before {
+				font-size: 1.25rem;
+				line-height: 80px;
+			}
+			&:hover {
+				background-color: ${props => props.theme.blue};
+				color: ${props => props.theme.white};
+			}
+		}
+		@media screen and (min-width: 1024px) {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			.list { 
+				padding: 0 10px;
+				border-top: 1px solid ${props => props.theme.blue};
+			}
+			a {
+				i:before {
+					font-size: 1.5rem;
+					line-height: 4.5rem;
+				}
+			}
+		}
 	}
 `;
 
 const Header = (props) => {
-	const menuUrls = [
-		{
-			name: 'Home',
-			url: '/dashboard',
-		},
-		{
-			name: 'Book List',
-			url: '/booklist',
-		},
-		{
-			name: 'Logout',
-			url: '/logout',
-		}
-	];
-	return(
+	const { header } = props;
+	return (
 		<StyledHeader>
 			<div className="logo">
-				<Link to="/dashboard"><img src={logo} alt="OER Bookr"/></Link>
+				<Link to="/dashboard"><img src={logo} alt="OER Bookr" /></Link>
 			</div>
-			{menuUrls.map(menu => (
-				<NavLink key={menu.url} to={menu.url} activeClassName="active">{menu.name}</NavLink>
-			))}
-			<div className="share">
-				<h3>Share</h3>
-				<a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="icon-facebook"></i></a>
-				<a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer"><i className="icon-twitter"></i></a>
-				<a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin"></i></a>
+			<div className="menu">
+				{header.menu.map(page => (
+					<NavLink key={page.url} to={page.url} activeClassName="active">{page.name}</NavLink>
+				))}
+			</div>
+			<div className="contacts">
+				<div className="list">
+					{header.contacts.map(link => <a key={link.type} href={link.url} target="_blank" rel="noopener noreferrer">
+						<i className={`icon-${link.type}`}></i>
+					</a>
+					)}
+				</div>
 			</div>
 		</StyledHeader>
 	)
 }
 
-export default Header
+const mapStateToProps = state => ({ header: state.header });
+export default connect(mapStateToProps)(Header);
