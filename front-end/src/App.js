1c3c1b3906b9c33f876f055ad755b7d7c401a58d
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-25 11:39:04 +0100

    Started responsive main container | Changed route names

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Route } from 'react-router-dom';
-import { Header, Login, SignUp, Dashboard, BookList, Book } from './components';
-
-import Registration from "./components/SignUp";
+import { Header, Login, Registration, Dashboard, BookList, Book } from './components';
 
 const StyledApp = styled.div`
+	max-width: 2160px;
+	margin: 0 auto;
 	main {
 		position: relative;
-		margin-left: auto;
-		max-width: calc(100% - 250px);
+		margin-top: 80px;
+		max-height: calc(100vh - 80px);
+		@media screen and (min-width: 1024px) {
+			margin-top: 0;
+			max-height: 100%;
+			margin-left: auto;
+			max-width: calc(100% - 250px);
+		}
 	}
 `;
 
@@ -21,7 +27,8 @@ const App = () => {
 				<Route path="/login" component={Login} />
 				<Route path="/signup" component={Registration} />
 				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/dashboard/:bookid" component={Book} />
+				<Route exact path="/books" component={BookList} />
+				<Route path="/books/:bookid" component={Book} />
 				{/* <Route path="/bookcard" component={BookCard} /> */}
 
 			</main>
