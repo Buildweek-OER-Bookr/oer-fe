ec7c85b7e8fb539c1249de92866b503f85c4339b
XAmi101 <famousvacancy@gmail.com>
2019-09-26 08:47:03 -0400

    fixed project structure

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Login from './Login';
-import SignUp from './SignUp';
-import Dashboard from './Dashboard';
-import BookList from './BookList';
-import Book from './Book';
-import Header from './Header';
-
-export { Header, Login, SignUp, Dashboard, BookList, Book };
